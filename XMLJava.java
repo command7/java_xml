8133c30bbb78995592871f9f2b43e45a65d2c8ac
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 16:11:35 -0500

    Outline for sax parsing

--- XMLJava.java
+++ XMLJava.java
@@ -1,2 +1,20 @@
-public class XMLJava {
+import org.xml.sax.*;
+import org.xml.sax.helpers.*;
+import java.io.*;
+
+public class XMLJava extends DefaultHandler{
+    public void startElement(String namespace,String localName, String qName, Attributes attrList )
+    {
+
+    }
+
+    public void endElement(String namespace,String localName, String qName)
+    {
+
+    }
+
+    public void characters(char[] ch, int start, int len)
+    {
+
+    }
 }
