8ff587549ef33d9542579bccc7a11ea7f27147c2
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 17:01:20 -0500

    Add setter methods for Results

--- Ratings.java
+++ Ratings.java
@@ -2,7 +2,7 @@ public class Ratings
 {
    private double AverageRating;
    private int TotalRatings;
-   private int TotalReviews
+   private int TotalReviews;
    private int lastReviewDate;
    private String lastReviewIntro;
 }
--- Results.java
+++ Results.java
@@ -1,13 +1,12 @@
-public class Results
-{
-   private String Title;
-   private String Address;
-   private String City;
-   private String State;
+public class Results {
+   private String title;
+   private String address;
+   private String city;
+   private String state;
    private String phone;
    private double latitude;
    private double longitude;
-   private Rating rating;
+   private Ratings rating;
    private double distance;
    private String url;
    private String clickUrl;
@@ -15,4 +14,56 @@ public class Results
    private String businessUrl;
    private String businessClickUrl;
    private Categories categories;
+   
+   public void setTitle(String _title) {
+      this.title = _title;
+   }
+   
+   public void setAddress(String _address) {
+      this.address = _address;
+   }
+   
+   public void setCity(String _city) {
+      this.city = _city;
+   }
+   
+   public void setState(String _state) {
+      this.state = _state;
+   }
+   
+   public void setPhone(String _phone) {
+      this.phone = _phone;
+   }
+   
+   public void setLatitude(double _lat) {
+      this.latitude = _lat;
+   }
+   
+   public void setLongitude(double _longitude) {
+      this.longitude = _longitude;
+   }
+   
+   public void setDistance(double _distance) {
+      this.distance = _distance;
+   }
+   
+   public void setUrl(String _url) {
+      this.url = _url;
+   }
+   
+   public void setClickUrl(String _clickUrl) {
+      this.clickUrl = _clickUrl;
+   }
+   
+   public void setMapUrl(String _mapUrl) {
+      this.mapUrl = _mapUrl;
+   }
+   
+   public void setBusinessUrl(String _busiUrl) {
+      this.businessUrl = _busiUrl;
+   }
+   
+   public void setBusinessClickUrl(String _bClickUrl) {
+      this.businessClickUrl = _bClickUrl;
+   }
 }
