a3ece82e82cebb979a3e8fc17ff5e8621fd841b5
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 16:50:28 -0500

    add attributes to Ratings

--- Ratings.java
+++ Ratings.java
@@ -1,3 +1,8 @@
 public class Ratings
 {
+   private double AverageRating;
+   private int TotalRatings;
+   private int TotalReviews
+   private int lastReviewDate;
+   private String lastReviewIntro;
 }
