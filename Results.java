2d28b416a4068de675dbe1ddacef0d86b2e34dbe
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 16:48:48 -0500

    Add attributes to Results

--- Results.java
+++ Results.java
@@ -1,3 +1,18 @@
 public class Results
 {
+   private String Title;
+   private String Address;
+   private String City;
+   private String State;
+   private String phone;
+   private double latitude;
+   private double longitude;
+   private Rating rating;
+   private double distance;
+   private String url;
+   private String clickUrl;
+   private String mapUrl;
+   private String businessUrl;
+   private String businessClickUrl;
+   private Categories categories;
 }
