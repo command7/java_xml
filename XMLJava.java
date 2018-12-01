6a41b0df7d7632dec2ca7cc50be92222c8b2aa11
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 16:20:50 -0500

    Add handler to test reading and writing xml elements and content

--- XMLJava.java
+++ XMLJava.java
@@ -3,18 +3,47 @@ import org.xml.sax.helpers.*;
 import java.io.*;
 
 public class XMLJava extends DefaultHandler{
-    public void startElement(String namespace,String localName, String qName, Attributes attrList )
+
+    public XMLJava()
     {
+        readFile();
 
     }
 
-    public void endElement(String namespace,String localName, String qName)
+    public void readFile()
     {
+        ResultHandler
+    }
+
+    public void startElement(String namespace,String localName, String sElement, Attributes attrList )
+    {
+        System.out.println("Starting Element : " + sElement)
 
     }
 
-    public void characters(char[] ch, int start, int len)
+    public void endElement(String namespace,String localName, String eElement)
     {
+        System.out.println("Ending Element : " + eElement);
+    }
+
+    public void characters(char[] content, int start, int len)
+    {
+        System.out.println("Content : " + content);
+    }
+
+    public static void main(String [] args)
+    {
+        try
+        {
+            XMLJava handler = new XMLJava();
+            XMLReader xr = XMLReaderFactory.createXMLReader();
+            xr.setContentHandler(handler);
+            xr.setErrorHandler(handler);
+
+            FileReader reader = new FileReader("RochesterSushi.xml");
+            xr.parse(new InputSource(reader));
+        }
+
 
     }
 }
