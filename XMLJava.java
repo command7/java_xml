b375a067c3e6dae5b5272b2a15bc700b1bdbf33b
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 16:40:49 -0500

    XML parser successfully reading data

--- XMLJava.java
+++ XMLJava.java
@@ -4,46 +4,48 @@ import java.io.*;
 
 public class XMLJava extends DefaultHandler{
 
-    public XMLJava()
-    {
-        readFile();
+//    public XMLJava()
+//    {
+//        readFile();
+//
+//    }
 
-    }
+//    public void readFile()
+//    {
+//        ResultHandler
+//    }
 
-    public void readFile()
-    {
-        ResultHandler
-    }
+   public void startElement(String namespace,String localName, String sElement, Attributes attrList )
+   {
+     System.out.println("Starting Element : " + sElement);
 
-    public void startElement(String namespace,String localName, String sElement, Attributes attrList )
-    {
-        System.out.println("Starting Element : " + sElement)
+   }
 
-    }
-
-    public void endElement(String namespace,String localName, String eElement)
-    {
-        System.out.println("Ending Element : " + eElement);
-    }
-
-    public void characters(char[] content, int start, int len)
-    {
-        System.out.println("Content : " + content);
-    }
-
-    public static void main(String [] args)
-    {
-        try
-        {
-            XMLJava handler = new XMLJava();
-            XMLReader xr = XMLReaderFactory.createXMLReader();
-            xr.setContentHandler(handler);
-            xr.setErrorHandler(handler);
-
-            FileReader reader = new FileReader("RochesterSushi.xml");
-            xr.parse(new InputSource(reader));
-        }
-
-
-    }
+   public void endElement(String namespace,String localName, String eElement)
+   {
+      System.out.println("Ending Element : " + eElement);
+   }
+   
+   public void characters(char[] content, int start, int len)
+   {
+      String text = new String(content, start, len );
+      System.out.println("Content : " + text);
+   }
+   
+   public static void main(String [] args)
+   {
+      try
+      {
+         XMLJava handler = new XMLJava();
+         XMLReader xr = XMLReaderFactory.createXMLReader();
+         xr.setContentHandler(handler);
+         xr.setErrorHandler(handler);
+   
+         FileReader reader = new FileReader("RochesterSushi.xml");
+         xr.parse(new InputSource(reader));
+      }
+      catch(Exception e) {
+         e.printStackTrace();
+      }   
+   }
 }
