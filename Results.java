29bbd663785e4f3a77b964f5c2060d6862d554fe
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 18:45:34 -0500

    add getter methods for Results

--- Results.java
+++ Results.java
@@ -66,4 +66,57 @@ public class Results {
    public void setBusinessClickUrl(String _bClickUrl) {
       this.businessClickUrl = _bClickUrl;
    }
+   
+   public void getTitle(String _title) {
+      this.title = _title;
+   }
+   
+   public void getAddress(String _address) {
+      this.address = _address;
+   }
+   
+   public void getCity(String _city) {
+      this.city = _city;
+   }
+   
+   public void getState(String _state) {
+      this.state = _state;
+   }
+   
+   public void getPhone(String _phone) {
+      this.phone = _phone;
+   }
+   
+   public void getLatitude(double _lat) {
+      this.latitude = _lat;
+   }
+   
+   public void getLongitude(double _longitude) {
+      this.longitude = _longitude;
+   }
+   
+   public void getDistance(double _distance) {
+      this.distance = _distance;
+   }
+   
+   public void getUrl(String _url) {
+      this.url = _url;
+   }
+   
+   public void getClickUrl(String _clickUrl) {
+      this.clickUrl = _clickUrl;
+   }
+   
+   public void getMapUrl(String _mapUrl) {
+      this.mapUrl = _mapUrl;
+   }
+   
+   public void getBusinessUrl(String _busiUrl) {
+      this.businessUrl = _busiUrl;
+   }
+   
+   public void getBusinessClickUrl(String _bClickUrl) {
+      this.businessClickUrl = _bClickUrl;
+   }
+
 }
