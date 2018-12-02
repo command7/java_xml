05d12a766240f0a8dd1d3c960a59d0d0bd133775
Vijay Raj Saravanan Radhakrishnan <vijay_saravanan@outlook.com>
2018-12-01 19:07:03 -0500

    Add getter and setter methods for Ratings

--- Ratings.java
+++ Ratings.java
@@ -1,8 +1,48 @@
 public class Ratings
 {
-   private double AverageRating;
-   private int TotalRatings;
-   private int TotalReviews;
+   private double averageRating;
+   private int totalRating;
+   private int totalReviews;
    private int lastReviewDate;
    private String lastReviewIntro;
+   
+   public double getAverageRating() {
+      return this.averageRating;
+   }
+   
+   public int getTotalRating() {
+      return this.totalRating;
+   }
+   
+   public int getTotalReviews() {
+      return this.totalReviews;
+   }
+   
+   public int getLastReviewDate() {
+      return this.lastReviewDate;
+   }
+   
+   public String getLastReviewIntro() {
+      return this.lastReviewIntro;
+   }
+   
+   public void setAverageRating(double _averageRating) {
+      this.averageRating = _averageRating;
+   }
+   
+   public void setTotalRating(int _totalRating) {
+      this.totalRating = _totalRating;
+   }
+   
+   public void setTotalReviews(int _totalReviews) {
+      this.totalReviews = _totalReviews;
+   }
+   
+   public void setLastReviewDate(int _lastReviewDate) {
+      this.lastReviewDate = _lastReviewDate;
+   }
+   
+   public void setLastReviewIntro(String _lastReviewIntro) {
+      this.lastReviewIntro = _lastReviewIntro;
+   }
 }
